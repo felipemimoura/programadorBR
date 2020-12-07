761d30da5b526ec9d0de2f5d50a5c7c63cbd8df0
felipemimoura <felipemimoura@gmail.com>
2020-12-07 18:20:26 -0300

    acompanhamento das aulas até metade do curso de js-basico

---
+++ javascript-basico/script.js
@@ -0,0 +1,115 @@
+//console.log('Teste ')
+
+//Criando variaveis
+/* var a = 'Placa de Captura'
+console.log(a)
+
+
+var b = "hd"
+b = a
+console.log(b) */
+
+//Tipos de dados
+
+/* var a = 'Palavra'
+console.log(a) */
+
+//Operadoração aritimetica
+// var a = 10
+// var b = 15
+
+// //soma
+// var c = a + b
+// console.log(c)
+// //subtração 
+// c = a - b
+// console.log("subtração:",c)
+
+// //Multiplicação
+// c = a * b
+// console.log('Multiplicação:' ,c)
+
+// //Divisão
+// c = a /b 
+// console.log('Divisão:', c)
+
+// //Modulo
+// c = a % b
+// console.log('Modulo', c)
+
+// //Incremento
+// a = a + 1 
+// console.log('Incremento', a)
+// //decremento
+// a = a - 1 
+// console.log('decremento', a)
+
+// console.log('Condicionais')
+// var idade = 17
+// if(idade >=18){
+//   console.log('Pode')
+// }else{
+//   console.log('Não pode')
+// }
+// console.log('Operadores Ternários')
+// var idade = 17
+// idade >= 18 ? console.log('Pode'): console.log('Não Pode') 
+
+// console.log('Switch')
+// var nota1 = 8.0
+// var nota2 = 8.0
+// var media = (nota1+nota2)/2
+// var conceito = ""
+
+// if(media >= 8){
+//   conceito = "Ótimo"
+// }else if(media >= 6.5){
+//   conceito = "Bom"
+// }else{
+//   conceito = "Regular"
+// }
+
+// switch(conceito){
+//   case "Ótimo":
+//     console.log('Parabens você é um ótimo aluno')
+//     break
+
+//   case "Bom":
+//     console.log('Você está quase perfeito')
+//     break
+  
+//   case "Regular":
+//     console.log('Estude mais um pouco')
+//     break
+
+//   default:
+//     console.log("Houve algum error")
+// }
+
+// console.log('Repetições')
+
+// var numero = 5
+
+// for(var vez = 0; vez <= numero ; vez++){
+//   console.log('Executando o for, pelo ' + vez + "vez")
+// }
+// console.log('Acabou')
+
+// console.log('Array')
+
+// console.log('Declando um aray')
+// //var alunos =  new Array('Igor', 'Jose', 'Marcos', 'Mariana')
+// var alunos = ['Igor', 'Jose', 'Marcos', 'Mariana']
+// console.log('Acessando elementos')
+// console.log(alunos[0])
+// console.log('Utilizando Array')
+// for(let i = 0; i < alunos.length; i++){
+//   console.log('Nome do Aluno: ' + alunos[i])
+// }
+
+console.log('Funções')
+function calculaMedia(nota1, nota2){
+  return media = (nota1 + nota2) / 2
+}
+
+console.log(calculaMedia(10,8))
