febc02e3c3d896d127b9a7191ff106b500f36de8
Felipe Moura <felipemimoura@gmail.com>
2021-01-20 20:07:08 -0300

    aula sobre promise

--- javascript-avancado/script.js
+++ javascript-avancado/script.js
@@ -24,27 +24,38 @@
 // }
 
 
-function ola(){
-    console.log('Olá')
-}
+// function ola(){
+//     console.log('Olá')
+// }
 
-function saudacao(s, nome){
-    s()
-    console.log(nome)
-}
+// function saudacao(s, nome){
+//     s()
+//     console.log(nome)
+// }
 
-saudacao(ola, "Felipe")
+// saudacao(ola, "Felipe")
 
 let usuarios = ["Adriano", "Marcia", "José"]
 
-function inserirUsario(nome, callback){
-    setTimeout(() =>{
-        usuarios.push(nome)
-        callback()
-    },1000)
-}
+function inserirUsario(nome){
+    let promise = new Promise(function(resolve, reject){
+        setTimeout(() =>{
+            usuarios.push(nome)
+
+            let error = false
+            if(!error){
+                resolve()
+            }else{
+                reject({msg: "erro de qualquer coisa"})
+            }
+            
+        },1000)
+    })
+        return promise
+    }
+
 
 function listarUsuario(){
     console.log(usuarios)
 }
-inserirUsario('Felipe',listarUsuario)
+inserirUsario('Felipe').then(listarUsuario)
