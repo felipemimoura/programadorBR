8412421cf4e4aa6d666a27a30bdcfdea60f02762
Felipe Moura <felipemimoura@gmail.com>
2021-01-20 19:28:07 -0300

    Acompanhamento da aula de this e bind

---
+++ javascript-avancado/script.js
@@ -0,0 +1,20 @@
+
+function speakGeneric(){
+    console.log(this.sound)
+}
+
+let dog = {
+    sound: "Au au",
+    speak: speakGeneric
+}
+
+let cat  = {
+    sound: "Miau",
+    speak: speakGeneric
+}
+
+let bindedFuntion = speakGeneric.bind(dog)
+
+dog.speak()
+cat.speak()
+bindedFuntion()
