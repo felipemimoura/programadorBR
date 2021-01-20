d704f24d3be09b7da42c2ed75c66d7068e7b6d48
Felipe Moura <felipemimoura@gmail.com>
2021-01-20 19:52:53 -0300

    aula sobre arrow functions

--- javascript-avancado/script.js
+++ javascript-avancado/script.js
@@ -1,20 +1,24 @@
+// function speakGeneric(){
+//     console.log(this.sound)
+// }
 
-function speakGeneric(){
-    console.log(this.sound)
+// let dog = {
+//     sound: "Au au",
+//     speak: speakGeneric
+// }
+
+// let cat  = {
+//     sound: "Miau",
+//     speak: speakGeneric
+// }
+
+// let bindedFuntion = speakGeneric.bind(dog)
+
+// dog.speak()
+// cat.speak()
+// bindedFuntion()
+
+
+let dobro = x =>{
+    console.log(2 * x)
 }
-
-let dog = {
-    sound: "Au au",
-    speak: speakGeneric
-}
-
-let cat  = {
-    sound: "Miau",
-    speak: speakGeneric
-}
-
-let bindedFuntion = speakGeneric.bind(dog)
-
-dog.speak()
-cat.speak()
-bindedFuntion()
